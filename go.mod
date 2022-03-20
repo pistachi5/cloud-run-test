be1bbee30acd29c06835187c6df5df1e0ea4927e
pistachi.o <pistachi.o@kakaocorp.com>
2022-03-21 00:31:18 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.daumkakao.com/awesomeProject
+
+go 1.16
+
+require cloud.google.com/go/logging v1.4.2
---
+++ main.go
@@ -0,0 +1,43 @@
+// Sample logging-quickstart writes a log entry to Cloud Logging.
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"cloud.google.com/go/logging"
+)
+
+func main() {
+	ctx := context.Background()
+
+	// Sets your Google Cloud Platform project ID.
+	projectID := "certain-benefit-335214"
+
+	// Creates a client.
+	client, err := logging.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	// Sets the name of the log to write to.
+	logName := "my-log"
+
+	// Selects the log to write to.
+	logger := client.Logger(logName)
+
+	// Sets the data to log.
+	text := "Hello, world!"
+
+	// Adds an entry to the log buffer.
+	logger.Log(logging.Entry{Payload: text})
+
+	// Closes the client and flushes the buffer to the Cloud Logging
+	// service.
+	if err := client.Close(); err != nil {
+		log.Fatalf("Failed to close client: %v", err)
+	}
+
+	fmt.Printf("Logged: %v\n", text)
+}
